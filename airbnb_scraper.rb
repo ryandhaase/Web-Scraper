3cf5bd8cdef3035bfae0d67f5ba86e6664fc91f9
Ryan Haase <ryandhaase@gmail.com>
2015-10-12 15:25:05 -0700

    Saving data to CSV file

--- airbnb_scraper.rb
+++ airbnb_scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'csv'
 
 # Store URL to be scraped
 url = 'https://www.airbnb.com/s/Brooklyn--NY--United-States'
@@ -7,15 +8,27 @@ url = 'https://www.airbnb.com/s/Brooklyn--NY--United-States'
 # Parse the page with Nokogiri
 page = Nokogiri::HTML(open(url))
 
-# Display output onto the screen
+# Store date in arrays
+name = []
 page.css('h3.h5.listing-name a').each do |line|
-  puts line.text.strip
+  name << line.text.strip
 end
 
+price = []
 page.css('span.h3.price-amount').each do |line|
-  puts line.text
+  price << line.text
 end
 
+details = []
 page.css('div.text-muted.listing-location.text-truncate').each do |line|
-  puts line.text
+  details << line.text
+end
+
+# Write data to CSV file
+CSV.open('airbnb_listings.csv', 'w') do |file|
+  file << ['Listing Name', 'Price', 'Details']
+  
+  name.length.times do |i|
+    file << [name[i], price[i], details[i]]
+  end
 end
