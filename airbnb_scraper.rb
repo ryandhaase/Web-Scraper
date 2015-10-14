8bb67e2ecb6fe928d6d5a34cda3829f18300a644
Ryan Haase <ryandhaase@gmail.com>
2015-10-13 22:19:59 -0700

    Modified code to identify max numbers of pages to be scraped

--- airbnb_scraper.rb
+++ airbnb_scraper.rb
@@ -2,26 +2,42 @@ require 'nokogiri'
 require 'open-uri'
 require 'csv'
 
-# Store URL to be scraped
-url = 'https://www.airbnb.com/s/Brooklyn--NY--United-States'
-
-# Parse the page with Nokogiri
+# Scrape the max numbers of pages and store in max_page variable
+url = 'https://www.airbnb.com/s/Brooklyn--NY--United-States?'
 page = Nokogiri::HTML(open(url))
 
-# Store date in arrays
+page_numbers = []
+page.css('div.pagination ul li a[target]').each do |line|
+  page_numbers << line.text
+end
+
+max_page = page_numbers[0...-1].map(&:to_i).max
+
+# Initialize empty arrays
 name = []
-page.css('h3.h5.listing-name a').each do |line|
-  name << line.text.strip
-end
-
 price = []
-page.css('span.h3.price-amount').each do |line|
-  price << line.text
-end
-
 details = []
-page.css('div.text-muted.listing-location.text-truncate').each do |line|
-  details << line.text.strip.split(/·/).map(&:strip)
+
+max_page.times do |i|
+
+  # Open search results page
+  url = "https://www.airbnb.com/s/Brooklyn--NY--United-States?page=#{i+1}"
+
+  # Parse the page with Nokogiri
+  page = Nokogiri::HTML(open(url))
+
+  # Store date in arrays
+  page.css('h3.h5.listing-name a').each do |line|
+    name << line.text.strip
+  end
+
+  page.css('span.h3.price-amount').each do |line|
+    price << line.text
+  end
+
+  page.css('div.text-muted.listing-location.text-truncate').each do |line|
+    details << line.text.strip.split(/·/).map(&:strip)
+  end
 end
 
 # Write data to CSV file
